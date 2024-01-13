f6687a64f2dcf4a0435655de04944c938da06984
unknown <Minskoleopc>
2024-01-13 21:21:09 +0530

    added

---
+++ script4.js
@@ -0,0 +1,85 @@
+
+// comparison operator 
+
+let a = 10
+console.log(typeof a)
+// 10 ,-10,45.5,-56.6
+
+let b = true
+console.log(b)
+console.log(typeof b)
+// true or false
+
+let c = "chinmay"
+console.log(c)
+console.log(typeof c)
+// 'A', 'a', '1','123asdsa#!@#',' '
+
+console.log(typeof 10) // value same -- type different 
+console.log(typeof '10') // value same - type different
+
+// < , > , <= , >= 
+// == , != -----> value
+// === , !==  -----> value and type 
+// entity < entity ------> boolean ----> true or false
+
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 !== '10')
+console.log(10 === 10)
+console.log(10 !== 10)
+console.log(10 != '10')
+console.log(10 != 10)
+console.log(12 > 2)
+console.log(12 < 2)
+console.log(12 >= 2)
+console.log(12 <= 2)
+console.log(12 >= 12)
+console.log(12 <= 12)
+console.log(12 == 12)
+
+// Logical operator 
+
+// AND  ---   && 
+// true  &&  true  =====> true
+// false &&  true  =====> false
+// true  &&  false =====> false
+// false &&  false =====> false
+
+
+
+
+// OR   ---   ||
+
+// true  ||  true  =====> true
+// false ||  true  =====> true
+// true  ||  false =====> true
+// false ||  false =====> false
+
+console.log(3 == 3 || 8 === 8)
+console.log(3 === '3' || 8 === 8)
+console.log(3 !== '3' || 8 !== 8)
+console.log(3 === '3' || 8 !== 8)
+
+
+// Not   ---  !
+
+console.log(!(1 == 1))
+console.log(!(1 == 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// logical operator
