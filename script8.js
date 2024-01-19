f7615ff5e48096064abdd2237a32b58b47ff076f
unknown <Minskoleopc>
2024-01-19 21:27:26 +0530

    added

---
+++ script8.js
@@ -0,0 +1,67 @@
+
+//               0       1      2      3
+let names = ["chinmay","amol","ram","sham"]
+
+// program 1 
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+
+//Object --- Properties and Methods
+names[0] = "samay"
+console.log(names)
+
+// Properties and method
+
+// Human  ----- amol
+// Properties - height , color , gender , age
+// Method - walk() , talk()
+
+// Gym()
+// Action  -- excercise
+// Return  -- health
+
+//             0        1        2           3
+let cities = ["pune","mumbai","bangalore","kolkata"]
+let q1  = cities.length
+console.log(q1)
+
+for(let i = 0 ; i < cities.length ; i++){ // 1 // 2 // 3 // 4
+    //console.log(i) // 0 // 1 //2 // 3
+    console.log(cities[i])
+}
+
+//               0        1          2     3
+let country = ["india","srilanka","japan","cuba"]
+for(let i = 0  ; i < country.length ; i++){
+    //console.log(i)
+    console.log(country[i])
+}
+
+for(let i = country.length -1 ; i >= 0 ; i--){
+    //console.log(i)
+    console.log(country[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
