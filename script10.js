b68b1fbe826572752d5f0a0efb4fc05b9848b1c3
unknown <Minskoleopc>
2024-01-23 21:25:04 +0530

    added

---
+++ script10.js
@@ -0,0 +1,91 @@
+//             0          1      2     3
+let names = ["poorva","sarika","sham","ram"]
+
+// Human
+// Properties --- color ,age , gender
+// Method - walk() , talk()
+
+// Method - Gym()
+// Action - Excercise()
+// Return  - Health 
+
+
+// length 
+//              0        1          2        3
+let cities = ["pune","banglore","kolkata","chennai"]
+let q1 = cities.length
+console.log(q1)
+
+// push()
+// Action -- add the element at the last 
+// return -- new length of array 
+
+let q2  = cities.push("nagpur")
+console.log(q2)
+console.log(cities)
+
+// unshift()
+// Action - add the element at start of array
+// return - returns new lenght of arrray
+let q3 = cities.unshift("wardha")
+console.log(q3)
+console.log(cities)
+// pop()
+// Action - removes the last element 
+// Return - returns the last elemnet
+let fruits = ["apple","mango","banana","orange"]
+let q4 = fruits.pop()
+console.log(fruits)
+
+// shift()
+let q5 = fruits.shift()
+console.log(q5)
+console.log(fruits)
+
+// Push("par") , unshift("par") , Pop() , Shift()
+
+// includes()
+//                   0            1          2
+let vegetables = ["cabbage","cauliflower",'brinjal']
+let q6  = vegetables.includes("Cabbage")
+console.log(q6)
+
+// indexOf()
+//               0          1         2
+let country = ["India","Pakistan","Bangladesh"]
+console.log(country)
+let q7 = country.indexOf("pakistan")
+console.log(q7)
+
+// at()
+country = ["India","Pakistan","Bangladesh"]
+let q8 = country.at(2)
+console.log(q8)
+
+//reverse()
+let q9 = country.reverse()
+console.log(country)
+
+// push() , unshift(), pop()  shift() 
+// includes() indexof() ,at() , reverse()
+// length
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
